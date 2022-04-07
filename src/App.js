0ffa27b284283cb6af1d65825859fc64098b77a3
Kingsley Onyeka Ifeanyi <80105176+Techillionaire@users.noreply.github.com>
2022-04-07 16:15:54 +0100

    taking a break

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import About from './components/About'
+import Footer from './components/Footer'
+// import Contact from './components/Contact'
 import Hero from './components/Hero'
 import Nav from './components/Nav'
+// import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
 
 const App = () => {
@@ -10,8 +13,10 @@ const App = () => {
       <Nav />
       <Hero />
       <About />
+      {/* <Projects /> */}
       <Testimonials />
-      
+      {/* <Contact /> */}
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FaGithub, FaFacebook, FaTwitter, FaInstagram, FaStackOverflow } from 'react-icons/fa'
+
+const Footer = () => {
+  return (
+    <footer className='bg-gray-200'>
+        <div className='container flex flex-col-reverse justify-between px-6 mx-auto space-y-8 md:flex-row md:space-y-0 md:py-12'>
+            <div className='flex flex-col-reverse items-center justify-between space-y-6 pb-6 md:flex-row md:justify-between md:space-x-80 md:space-y-0 '>
+                <div>
+                    <a href='/' className='logo font-light text-center text-base space-y-4 md:font-bold md:font-2xl'>Techillionaire Inc.</a>
+                </div>
+                <div className='flex justify-between space-x-12 py-6 md:space-x-40'>
+                    <a href='https://github.com/Techillionaire' target='_blank' className=''>
+                        <i><FaGithub /></i>
+                    </a>
+                    <a href='https://stackoverflow.com/users/15582692/techillionaire' target='_blank' className=''>
+                        <i><FaStackOverflow /></i>
+                    </a>
+                    <a target='_blank' href='https://twitter.com/leeowonikoko' className=''>
+                        <i><FaTwitter /></i>
+                    </a>
+                    <a target='_blank' href='https://www.instagram.com/leeowonikoko/' className=''>
+                        <i><FaInstagram /></i>
+                    </a>
+                    <a  target='_blank' href='https://www.facebook.com/leeowonikoko1' className=''>
+                        <i><FaFacebook /></i>
+                    </a>
+                </div>
+
+        
+            </div>
+        </div>
+
+    </footer>
+  )
+}
+
+export default Footer
